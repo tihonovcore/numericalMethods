44108d58741c74d155abf1cfed1f6a0bbf3ed86e
Vitaly <tihonovcore@gmail.com>
2020-06-06 11:18:04 +0300

    clear main & readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-###Задание 1
-[результаты](https://docs.google.com/spreadsheets/d/1BkqafVvGAzhTBP4P0YMdeVqq_7lXGjmYXcjaHzP9704/edit?usp=sharing)
+### Задание 1
+[Результаты](https://docs.google.com/spreadsheets/d/1BkqafVvGAzhTBP4P0YMdeVqq_7lXGjmYXcjaHzP9704/edit?usp=sharing)
+
+### Задание 2
+[Результаты]()
+
+### Задание 3
+[Результаты]()
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,10 +5,10 @@ fun main() {
     for (t in 350 .. 650) {
         val kelvin = t + 273.15
 
-        val (y1, y2, y3) = findSolution(kelvin)
-        val s1 = stream(AlCl,  kelvin, y1)
-        val s2 = stream(AlCl2, kelvin, y2)
-        val s3 = stream(AlCl3, kelvin, y3)
+        val (x1, x2, x3) = findSolution(kelvin)
+        val s1 = stream(AlCl,  kelvin, x1)
+        val s2 = stream(AlCl2, kelvin, x2)
+        val s3 = stream(AlCl3, kelvin, x3)
 
         val (s1abs, s2abs, s3abs) = listOf(s1, s2, s3).map { it.absoluteValue }
         val speed = (s1 + s2 + s3) * (26.9815 / 2690) * 1e9
